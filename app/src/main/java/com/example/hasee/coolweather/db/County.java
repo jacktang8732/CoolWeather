13d67d42e2f2b6c8a0412198bed9cfe4ed1ca23f
tangjackaa <jacktang8732@gmail.com>
2018-02-25 17:37:04 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/hasee/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.hasee.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by hasee on 2018/2/25.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
